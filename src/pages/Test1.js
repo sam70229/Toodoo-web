75e41ccfe3aba6ed56326f5ee0c2b68c706cc4e1
sam70229 <sam70229@gmail.com>
2021-09-30 16:37:55 +0800

    Update Test1.js

--- src/pages/Test1.js
+++ src/pages/Test1.js
@@ -4,7 +4,7 @@ export default function Test1() {
     return (
         <React.Fragment>
             <h1>
-                This is test1
+                This is test2 sub1
             </h1>
         </React.Fragment>
     )
