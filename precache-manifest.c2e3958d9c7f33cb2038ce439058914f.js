5ceca9636de64251119b5a0b1a955801b8abc567
sam70229 <sam70229@gmail.com>
2021-09-30 16:30:22 +0800

    Updates

---
+++ precache-manifest.c2e3958d9c7f33cb2038ce439058914f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bba8b04d3b43ea00f0c9d219435481",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c60554281119fe8bda84",
+    "url": "/react-app/static/css/main.87113640.chunk.css"
+  },
+  {
+    "revision": "40c274b2d8ba17f27f19",
+    "url": "/react-app/static/js/2.3efdf7db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.3efdf7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60554281119fe8bda84",
+    "url": "/react-app/static/js/main.cf884c85.chunk.js"
+  },
+  {
+    "revision": "aff7883fb035802b6c2f",
+    "url": "/react-app/static/js/runtime-main.06bc5747.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
